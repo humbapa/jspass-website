f600799d42e9c03a53191c4fcbdb534c967a3e1b
Blumenthal Martin <martin.blumenthal@ti8m.ch>
2020-03-27 17:08:39 +0100

    Theming and first menu-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jspass-website';
+export class AppComponent implements OnInit {
+  title = 'JSPass';
+
+  toggleVersionStatus = false;
+  version = 2;
+  legacyIconDisplay = 'none';
+
+  toggleThemeStatus = false;
+  theme = 'light';
+
+  ngOnInit() {
+    const version = window.localStorage.getItem('version');
+    if (version && Number(version) !== this.version) {
+      this.version = 1;
+      this.updateVersion();
+    }
+
+    const theme = window.localStorage.getItem('theme');
+    if (theme && theme !== this.theme) {
+      this.theme = 'dark';
+      this.updateTheme();
+    }
+  }
+
+  toggleVersion(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (this.toggleVersionStatus) {
+      this.version = 2;
+    } else {
+      this.version = 1;
+    }
+
+    this.updateVersion();
+  }
+
+  updateVersion() {
+    if (this.version === 2) {
+      setTimeout(() => (this.toggleVersionStatus = false));
+      this.legacyIconDisplay = 'none';
+    } else {
+      setTimeout(() => (this.toggleVersionStatus = true));
+      this.legacyIconDisplay = 'inline';
+    }
+
+    window.localStorage.setItem('version', `${this.version}`);
+  }
+
+  toggleTheme(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (this.toggleThemeStatus) {
+      this.theme = 'light';
+    } else {
+      this.theme = 'dark';
+    }
+
+    this.updateTheme();
+  }
+
+  updateTheme() {
+    if (this.theme === 'light') {
+      setTimeout(() => (this.toggleThemeStatus = false));
+      document.body.classList.remove('jspass-dark-theme');
+      document.body.classList.add('jspass-light-theme');
+    } else {
+      setTimeout(() => (this.toggleThemeStatus = true));
+      document.body.classList.remove('jspass-light-theme');
+      document.body.classList.add('jspass-dark-theme');
+    }
+
+    window.localStorage.setItem('theme', this.theme);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +10,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatChipsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(overlayContainer: OverlayContainer) {
+    overlayContainer.getContainerElement().classList.add('jspass-dark-theme');
+  }
+}
